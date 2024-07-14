917497e0901ac4090feb183c8ed93ccda6973df2
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-07-15 00:19:22 +1000

    Rewrite encryption.py with DES

--- encryption.py
+++ encryption.py
@@ -7,22 +7,20 @@ CP2414 - Task 4
 By Zhu Dawei
 """
 import random
-import hashlib
+from Crypto.Cipher import DES, AES
+from Crypto.Util.Padding import pad
 
 
-def encrypt_password(raw_string, salt):
+def encrypt_password(data: str, salt: bytes) -> object:
     """
     To get a string encrypted with the sha256 and salt.
-    :param raw_string:
+    :param key:
     :param salt:
     :return:
     """
-    # salt = produce_salt(0, 1)
-    string_hashed = hashlib.sha256(raw_string.encode('UTF-8'))
-    # Add salt
-    # According to python handbook, (string_hashed.update) == (string_hashed(raw_string + salt))
-    string_hashed.update(str(salt).encode())
-    return string_hashed.hexdigest()
+    cipher = DES.new(salt, DES.MODE_ECB)
+    cipher_pad = cipher.encrypt(data.encode('utf-8'))
+    return cipher
 
 
 def generate_salt(a, b):
@@ -36,27 +34,10 @@ def generate_salt(a, b):
 
 
 def main():
-    """Demonstration of the module."""
-    raw_string = 'Hello'
-    print('raw_string1:', raw_string)
-
-    string_hashed1 = hashlib.sha256(raw_string.encode())
-    raw_string2 = 'Hello'
-    print('raw_string2:', raw_string2)
-    string_hashed2 = hashlib.sha256(raw_string2.encode('UTF-8'))
-    print('string_hashed1:', string_hashed1.hexdigest())
-    print('string_hashed2:', string_hashed2.hexdigest())
-    print('Is string1 == string2?', string_hashed1.hexdigest() == string_hashed2.hexdigest())
-    print('=' * 36)
-    salt = generate_salt(0, 1)
-    string_encrypted = encrypt_password(raw_string, salt)
-    string_hashed_encrypted = encrypt_password(raw_string, salt)
-    print(f'salt: {salt}')
-    print('string_hashed1:', string_hashed1.hexdigest())
-    print('string_encrypted:', string_encrypted)
-    print('Is raw_string1 == string_encrypted?', string_hashed1.hexdigest() == string_hashed_encrypted)
-
-    print('\n=== RSA ===')
+    text = '12345678'
+    salt = b'12345678'
+    cipher = encrypt_password(text, salt)
+    print(cipher)
 
 
 if __name__ == '__main__':
