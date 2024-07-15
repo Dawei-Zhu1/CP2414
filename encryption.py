5cd878513e682275f9123210a1cd1f99b8681248
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-07-15 10:44:56 +1000

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -7,37 +7,53 @@ CP2414 - Task 4
 By Zhu Dawei
 """
 import random
+import math
 from Crypto.Cipher import DES, AES
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
+from input_checking import MAXIMUM_LENGTH
 
 
-def encrypt_password(data: str, salt: bytes) -> object:
+def encrypt_password(data: str, key: bytes) -> bytes:
     """
     To get a string encrypted with the sha256 and salt.
-    :param key:
-    :param salt:
-    :return:
+    :param data: Password to encrypt.
+    :param key: Salt.
+    :return: The encrypted password.
     """
-    cipher = DES.new(salt, DES.MODE_ECB)
-    cipher_pad = cipher.encrypt(data.encode('utf-8'))
-    return cipher
+    cipher = DES.new(key, DES.MODE_ECB)
+    cipher_size = set_block_size(MAXIMUM_LENGTH)
+    cipher_text = cipher.encrypt(pad(data.encode('utf-8'), 24))
+    return cipher_text
 
 
 def generate_salt(a, b):
     """
     Generate a random salt.
-    :param a:
-    :param b:
+    :param a: Salt minimum.
+    :param b: Salt maximum.
     :return:
     """
     return random.uniform(a, b)
 
 
+def set_block_size(password_length: int) -> int:
+    """
+    Set block size with password length.
+    """
+    return math.ceil(password_length / 8) * DES.block_size
+
+
 def main():
-    text = '12345678'
-    salt = b'12345678'
-    cipher = encrypt_password(text, salt)
-    print(cipher)
+    key = b'12345678'
+    text = 'Helloooo'
+    cipher = DES.new(key, DES.MODE_ECB)
+    cipher_text = encrypt_password(text, key)
+    print(cipher_text)
+    decrypted = unpad(
+        cipher.decrypt(cipher_text),
+        set_block_size(MAXIMUM_LENGTH)
+    )
+    print(decrypted, DES.block_size * 3)
 
 
 if __name__ == '__main__':
