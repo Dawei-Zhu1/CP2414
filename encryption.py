b894b1a7a69c2e4859fa640996260ed9e1b8d929
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2024-07-19 00:16:03 +1000

    Fix an issue

--- encryption.py
+++ encryption.py
@@ -20,16 +20,17 @@ def generate_keys(a: int) -> (rsa.PrivateKey, rsa.PublicKey):
     return rsa.newkeys(a)
 
 
-def encrypt_password(data: any, key: rsa.PublicKey) -> str:
+def encrypt_password(message: any, key: rsa.PublicKey) -> str:
     """
     To get a string encrypted with the sha256 and salt.
-    :param data: Password to encrypt.
+    :param message: Password to encrypt.
     :param key: rsa key.
     :return: The encrypted password in hex.
     """
 
     _key = key
-    cipher_text = rsa.encrypt(data, _key)
+    _message = bytes(message.encode('utf-8'))
+    cipher_text = rsa.encrypt(_message, _key)
     return cipher_text.hex()
 
 
@@ -63,7 +64,7 @@ def validate_password(raw_string: str, key: str, stored_password: str) -> bool:
     _stored_password = stored_password
     _key = import_key(key)
     # Encrypt the raw string
-    cipher_text_to_be_verified = encrypt_password(password_to_be_verified, key)
+    cipher_text_to_be_verified = encrypt_password(password_to_be_verified, _key)
     return cipher_text_to_be_verified == _stored_password
 
 
